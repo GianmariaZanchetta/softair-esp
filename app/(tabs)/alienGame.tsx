54892960fa5d87c7001c52027bc4ec914276e137
GianmariaZanchetta <gianmariazanchetta2003@gmail.com>
2026-05-21 12:00:41 +0200

    tastiera caratteri runici

--- app/(tabs)/alienGame.tsx
+++ app/(tabs)/alienGame.tsx
@@ -1,50 +1,14 @@
 import { Pressable, Text, View, TextInput, Button, FlatList, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
-
+import * as data from '../../assets/fonts/alienFont.js'
 
 
 
 export default function alienGame() {
 
     const [text, onChangeText] = React.useState("")
-    const DATA = [
-        {
-            id: 'a',
-            title: 'a',
-        },
-        {
-            id: 'b',
-            title: 'b',
-        },
-        {
-            id: 'c',
-            title: 'c',
-        },
-        {
-            id: 'd',
-            title: 'd',
-        },
-        {
-            id: 'e',
-            title: 'e',
-        },
-        {
-            id: 'f',
-            title: 'f',
-        },
-        {
-            id: 'g',
-            title: 'g',
-        },
-        {
-            id: 'h',
-            title: 'h',
-        },
-        {
-            id: 'i',
-            title: 'i',
-        },
-    ];
+
+    const DATA = data.alienFont
 
     type ItemProps = {title: string};
 
@@ -57,13 +21,7 @@ export default function alienGame() {
 
     return(
         <View
-            style={{
-                flex: 1,
-                backgroundColor: "rgb(56, 56, 56)",
-                margin: 0,
-                padding: 0,
-                alignItems: "center"
-        }}>
+            style={styles.container}>
             <Text
                 style={{
                       marginTop: 40,
@@ -76,31 +34,73 @@ export default function alienGame() {
                 Inserisci il codice
             </Text>
 
-            <TextInput
+            <View style={{flexDirection: 'row'}}>
+                <TextInput
+                        style={{
+                            height: 45,
+                            width: 210,
+                            marginTop: 20,
+                            borderWidth: 1,
+                            padding: 10,
+                            //borderRadius: 10,
+                            borderColor: "rgb(255, 255, 255)",
+                            color: 'white',
+                            textAlign: "center",
+                            fontSize: 20,
+                            backgroundColor: 'rgb(15, 15, 15)',
+                            marginLeft: 220,
+                        }}
+                        editable={false}
+                        onChangeText={onChangeText}
+                        value={text}
+                        //placeholder="123456"
+                        placeholderTextColor= "rgba(121, 121, 121, 0.73)"
+                ></TextInput>
+
+                <Pressable
+                    onPress={()=>onChangeText(text.slice(0, -1))}
                     style={{
-                      height: 45,
-                      width: 210,
-                      marginTop: 20,
-                      borderWidth: 1,
-                      padding: 10,
-                      //borderRadius: 10,
-                      borderColor: "rgb(255, 255, 255)",
-                      color: 'white',
-                      textAlign: "center",
-                      fontSize: 20,
-                      backgroundColor: 'rgb(15, 15, 15)',
+                        marginLeft: 60,
+                        backgroundColor: '#797979',
+                        paddingBottom: 20,
+                        paddingHorizontal: 60,
                     }}
-                    onChangeText={onChangeText}
-                    value={text}
-                    placeholder="123456"
-                    placeholderTextColor= "rgba(121, 121, 121, 0.73)"
-            
-                  ></TextInput>
+                >
+                    <Text
+                        style={{
+                            color: '#ffffff',
+                            fontSize: 45,
+                            padding: 0,
+                            margin: 0,
+                        }}
+                    >←</Text>
+                </Pressable>
+            </View>
+
 
             <FlatList
+                horizontal={false}
                 style={[styles.flatList]}
                 data={DATA}
-                renderItem={({item}) => <Item title={item.title} />}
+                numColumns={9}
+                contentContainerStyle={{
+                    alignItems: 'center',
+                    //borderColor: "red",
+                    //borderWidth: 1,
+                    width: 600,
+                    height: 205,
+                    marginTop: 30,
+                    
+                }}
+                renderItem={({item}) => 
+                    <Pressable
+                     onPress={()=>{onChangeText(text + item.id)}}
+                    >
+                        <Item 
+                            title={item.title}
+                            
+                        />
+                    </Pressable>}
                 keyExtractor={item => item.id}
             />
 
@@ -112,21 +112,28 @@ export default function alienGame() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "rgb(56, 56, 56)",
+    margin: 0,
+    padding: 0,  
+    alignItems: "center",
+    textAlign: "center",
+    flexGrow: 1,
+    justifyContent: "center",
+    
   },
   flatList: {
-    borderColor: "red",
-    borderWidth: 1,
-    flexDirection: "row",
+    
   },
   item: {
     
-    backgroundColor: '#f9c2ff',
+    backgroundColor: '#70c08900',
     padding: 10,
     marginVertical: 4,
     marginHorizontal: 16,
-
+    height: 60
   },
   title: {
     fontSize: 32,
+    color: '#ffffff',
   },
 });
---
+++ assets/fonts/alienFont.js
@@ -0,0 +1,122 @@
+export const alienFont = [
+        {
+            id: 'ᚠ',
+            title: 'ᚠ',
+            value: 'f'
+        },
+        {
+            id: 'ᚢ',
+            title: 'ᚢ',
+            value: 'u'
+        },
+        {
+            id: 'ᚦ',
+            title: 'ᚦ',
+            value: 'þ'
+        },
+        {
+            id: 'ᚫ',
+            title: 'ᚫ',
+            value: 'a'
+        },
+        {
+            id: 'ᚱ',
+            title: 'ᚱ',
+            value: 'r'
+        },
+        {
+            id: 'ᚲ',
+            title: 'ᚲ',
+            value: 'k'
+        },
+        {
+            id: 'ᚷ',
+            title: 'ᚷ',
+            value: 'g'
+        },
+        {
+            id: 'ᚹ',
+            title: 'ᚹ',
+            value: 'v'
+        },
+        {
+            id: 'ᚺ',
+            title: 'ᚺ',
+            value: 'h'
+        },
+        {
+            id: 'ᚾ',
+            title: 'ᚾ',
+            value: 'n'
+        },
+        {
+            id: 'ᛁ',
+            title: 'ᛁ',
+            value: 'i'
+        },
+        {
+            id: 'ᛃ',
+            title: 'ᛃ',
+            value: 'j'
+        },
+        {
+            id: 'ᛇ',
+            title: 'ᛇ',
+            value: 'æ'
+        },
+        {
+            id: 'ᛈ',
+            title: 'ᛈ',
+            value: 'p'
+        },
+        {
+            id: 'ᛉ',
+            title: 'ᛉ',
+            value: 'z'
+        },
+        {
+            id: 'ᛋ',
+            title: 'ᛋ',
+            value: 's'
+        },
+        {
+            id: 'ᛏ',
+            title: 'ᛏ',
+            value: 't'
+        },
+        {
+            id: 'ᛒ',
+            title: 'ᛒ',
+            value: 'b'
+        },
+        {
+            id: 'ᛖ',
+            title: 'ᛖ',
+            value: 'e'
+        },
+        {
+            id: 'ᛗ',
+            title: 'ᛗ',
+            value: 'm'
+        },
+        {
+            id: 'ᛚ',
+            title: 'ᛚ',
+            value: 'l'
+        },
+        {
+            id: 'ᛜ',
+            title: 'ᛜ',
+            value: 'ng'
+        },
+        {
+            id: 'ᛞ',
+            title: 'ᛞ',
+            value: 'd'
+        },
+        {
+            id: 'ᛟ',
+            title: 'ᛟ',
+            value: 'o'
+        },
+    ];
